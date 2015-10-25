1754965d2f36898c7b02d7d873d7663c600b3afd
kopaka7 <Naren@s172-20-13-h28.paws.uga.edu>
2015-10-25 06:29:04 -0400

    Assorted changes

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -8,6 +8,6 @@ public class DemoServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().println("{ \"name\": \”This project is in progress\” }");
+        resp.getWriter().println("{ \"name\": \"This project is in progress\" }");
     }
 }
